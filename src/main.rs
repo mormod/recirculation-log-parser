08dd772626dca86072e776ba5f47e6599a151590
Moritz R. Huellmann <huellmann@embedded.rwth-aachen.de>
2023-04-02 09:50:26 +0200

    Put datasets in own group for annotations, add blosc

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+
+
+use std::{path::PathBuf, time::SystemTime, collections::{HashMap, HashSet}, ops::Deref};
+
 mod parsers;
-
-use std::{path::PathBuf, time::SystemTime, collections::{HashMap, HashSet}};
-
 use parsers::{
     parse_canids, CanId, 
     parse_comments, TVComment, 
@@ -23,6 +24,63 @@ impl CanMsgCollection {
     }
 }
 
+fn create_str_attr<T>(location: &T, name: &str, value: &str) -> hdf5::Result<()>
+where
+    T: Deref<Target = hdf5::Location>,
+{
+    let attr = location.new_attr::<hdf5::types::VarLenUnicode>().create(name)?;
+    let value_: hdf5::types::VarLenUnicode = value.parse().unwrap();
+    attr.write_scalar(&value_)
+}
+
+fn write_to_h5(collections: &Vec<CanMsgCollection>) -> hdf5::Result<()> {
+
+    #[cfg(feature = "blosc")]
+    hdf5::filters::blosc_set_nthreads(4);
+
+    let root = hdf5::File::create("canlog.h5")?;
+    create_str_attr::<hdf5::Group>(&root, "created", chrono::Local::now().to_rfc3339().as_str())?;
+
+    for collection in collections {
+        let str_id = match &collection.can_id.str_id {
+            Some(str_id) => str_id.to_owned(),
+            None => collection.can_id.hex_id.to_string()
+        };
+        
+        let subgroup = root.create_group(str_id.as_str())?;
+        
+        subgroup.new_attr::<u32>().create("hex_id")?.write_scalar(&collection.can_id.hex_id)?;
+        
+        let desc = match &collection.can_id.description {
+            Some(desc) => desc.as_str(),
+            None => "None"
+        };
+        create_str_attr::<hdf5::Group>(&subgroup, "description", desc)?;
+
+        let unit = match &collection.can_id.unit {
+            Some(unit) => unit.as_str(),
+            None => "None"
+        };
+        create_str_attr::<hdf5::Group>(&subgroup, "unit", unit)?;
+
+        let scale = match &collection.can_id.scale {
+            Some(scale) => scale,
+            None => &1.0
+        };
+        subgroup.new_attr::<f32>().create("scale")?.write_scalar(scale)?;
+
+        let dataset = subgroup.new_dataset_builder();
+        
+        #[cfg(feature = "blosc")]
+        let dataset = dataset.blosc_zstd(9, true); // zstd + shuffle
+
+        let dataset = dataset.with_data(&collection.collection);
+        dataset.create(format!("{:#?}", str_id).as_str())?;
+    }
+
+    Ok(())
+}
+
 fn main() {
     let can_ids: Vec<CanId> = parse_canids(&PathBuf::from("src/ids.txt"));
     let mut all_can_ids = HashMap::new();
@@ -30,10 +88,10 @@ fn main() {
         all_can_ids.insert(can_id.hex_id, can_id);
     }
 
-    let comments: Vec<TVComment> = parse_comments(&PathBuf::from("src/cmt.txt"));
-    for comment in comments {
-        println!("{:#?}", comment);
-    }
+    // let comments: Vec<TVComment> = parse_comments(&PathBuf::from("src/cmt.txt"));
+    // for comment in comments {
+    //     println!("{:#?}", comment);
+    // }
 
     let start = SystemTime::now();
     let mut messages: Vec<CanMsg> = parse_messages(&PathBuf::from("src/log.txt"));
@@ -67,6 +125,8 @@ fn main() {
             split_by_id.last_mut().unwrap().collection.push(msg.to_owned());
         }
     }
+
+    let _ = write_to_h5(&split_by_id);
 
     println!("Not mappable:");
     for nm in not_mappable_ids {
